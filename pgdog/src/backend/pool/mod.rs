ccb522f8fe0560ca2d3946bc7648748a003c48d0
Lev Kokotov <lev.kokotov@gmail.com>
2025-01-18 22:10:01 -0800

    pool module rename

--- pgdog/src/backend/pool/mod.rs
+++ pgdog/src/backend/pool/mod.rs
@@ -11,7 +11,7 @@ pub mod guard;
 pub mod healthcheck;
 pub mod inner;
 pub mod monitor;
-pub mod pool;
+pub mod pool_impl;
 pub mod replicas;
 pub mod shard;
 pub mod stats;
@@ -24,13 +24,13 @@ pub use error::Error;
 pub use guard::Guard;
 pub use healthcheck::Healtcheck;
 use monitor::Monitor;
-pub use pool::Pool;
+pub use pool_impl::Pool;
 pub use replicas::Replicas;
 pub use shard::Shard;
 
 use ban::Ban;
 use inner::Inner;
-use pool::Mapping;
+use pool_impl::Mapping;
 
 #[cfg(test)]
 mod test;
