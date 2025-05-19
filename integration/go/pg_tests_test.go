662bc53e5d3d95d15b0becba26d1b7171c018525
Lev Kokotov <levkk@users.noreply.github.com>
2025-05-19 09:01:42 -0700

    More golang tests (#174)

--- integration/go/pg_tests_test.go
+++ integration/go/pg_tests_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"testing"
 	"time"
@@ -27,8 +28,15 @@ func assertNoOutOfSync(t *testing.T) {
 			panic(err)
 		}
 
-		out_of_sync := values[16].(int64)
-		assert.Equal(t, out_of_sync, int64(0), "No connections should be out of sync")
+		for i, description := range rows.FieldDescriptions() {
+			if description.Name == "out_of_sync" {
+				out_of_sync := values[i].(int64)
+				assert.Equal(t, out_of_sync, int64(0), "No connections should be out of sync")
+				return
+			}
+		}
+
+		panic("No out_of_sync column in SHOW POOLS")
 	}
 }
 
@@ -42,33 +50,73 @@ func connectNormal() (*pgx.Conn, error) {
 	return conn, nil
 }
 
-func TestConnect(t *testing.T) {
-	conn, err := connectNormal()
+func connectSharded() (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), "postgres://pgdog:pgdog@127.0.0.1:6432/pgdog_sharded")
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Can't connect: %v\n", err)
+		return nil, err
 	}
-	defer conn.Close(context.Background())
 
-	assertNoOutOfSync(t)
-
+	return conn, nil
 }
 
-func TestSelect(t *testing.T) {
-	conn, err := connectNormal()
+func connectBoth() []*pgx.Conn {
+	// conns := make([]*pgx.Conn, 2)
+
+	normal, err := connectNormal()
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close(context.Background())
-
-	for i := range 25 {
-		var one int64
-		err = conn.QueryRow(context.Background(), "SELECT $1::bigint AS one", i).Scan(&one)
-		if err != nil {
-			panic(err)
-		}
-		assert.Equal(t, one, int64(i))
+	sharded, err := connectSharded()
+	if err != nil {
+		panic(err)
 	}
 
+	conns := []*pgx.Conn{normal, sharded}
+
+	return conns
+}
+
+func TestConnect(t *testing.T) {
+	for _, conn := range connectBoth() {
+		conn.Close(context.Background())
+	}
+
+	assertNoOutOfSync(t)
+}
+
+func TestSelect(t *testing.T) {
+	conns := connectBoth()
+
+	for _, conn := range conns {
+		for i := range 25 {
+			var one int64
+			var len int
+			rows, err := conn.Query(context.Background(), "SELECT $1::bigint AS one", i)
+			if err != nil {
+				panic(err)
+			}
+
+			for rows.Next() {
+				len += 1
+				values, err := rows.Values()
+
+				if err != nil {
+					panic(err)
+				}
+
+				one = values[0].(int64)
+				assert.Equal(t, one, int64(i))
+			}
+
+			assert.Equal(t, len, 1)
+		}
+
+		conn.Close(context.Background())
+	}
+
+	assertNoOutOfSync(t)
 }
 
 func TestTimeout(t *testing.T) {
@@ -95,35 +143,117 @@ func TestTimeout(t *testing.T) {
 }
 
 func executeTimeoutTest(t *testing.T) {
-	conn, err := connectNormal()
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close(context.Background())
+	conns := connectBoth()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
-	defer cancel()
+	for _, conn := range conns {
+		ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+		defer cancel()
 
-	c := make(chan int, 1)
+		c := make(chan int, 1)
 
-	go func() {
-		err = pgSleepOneSecond(conn)
-		if err == nil {
-			panic(err)
+		go func() {
+			err := pgSleepOneSecond(conn, ctx)
+			assert.NotNil(t, err)
+
+			defer conn.Close(context.Background())
+
+			c <- 0
+		}()
+
+		select {
+		case <-c:
+			t.Error("Context should of been cancelled")
+		case <-ctx.Done():
 		}
-
-		c <- 0
-	}()
-
-	select {
-	case <-c:
-		t.Error("Context should of been cancelled")
-	case <-ctx.Done():
 	}
+
 }
 
 // Sleep for 1 second.
-func pgSleepOneSecond(conn *pgx.Conn) (err error) {
-	_, err = conn.Exec(context.Background(), "SELECT pg_sleep(1)")
+func pgSleepOneSecond(conn *pgx.Conn, ctx context.Context) (err error) {
+	_, err = conn.Exec(ctx, "SELECT pg_sleep(1)")
 	return err
+}
+
+func TestCrud(t *testing.T) {
+	conns := connectBoth()
+
+	for _, conn := range conns {
+		defer conn.Close(context.Background())
+	}
+
+	for _ = range 25 {
+		for _, conn := range conns {
+			id := rand.Intn(1_000_000)
+			rows, err := conn.Query(context.Background(), "INSERT INTO sharded (id) VALUES ($1) RETURNING *", id)
+
+			assert.Nil(t, err)
+
+			for rows.Next() {
+				values, err := rows.Values()
+				assert.Nil(t, err)
+				assert.Equal(t, int64(id), values[0].(int64))
+			}
+
+			rows, err = conn.Query(context.Background(), "SELECT * FROM sharded WHERE id = $1", id)
+
+			var len int
+
+			for rows.Next() {
+				values, err := rows.Values()
+				assert.Nil(t, err)
+				len += 1
+				assert.Equal(t, values[0].(int64), int64(id))
+			}
+
+			assert.Equal(t, len, 1)
+
+			cmd, err := conn.Exec(context.Background(), "DELETE FROM sharded WHERE id = $1", id)
+			assert.Nil(t, err)
+			assert.True(t, cmd.Delete())
+			assert.Equal(t, cmd.RowsAffected(), int64(1))
+		}
+	}
+}
+
+func TestTransactions(t *testing.T) {
+	conns := connectBoth()
+
+	for _, conn := range conns {
+		defer conn.Close(context.Background())
+	}
+
+	for _ = range 25 {
+		for _, conn := range conns {
+			tx, err := conn.BeginTx(context.Background(), pgx.TxOptions{})
+			assert.Nil(t, err)
+			defer tx.Rollback(context.Background())
+
+			id := rand.Intn(1_000_000)
+
+			rows, err := tx.Query(context.Background(), "INSERT INTO sharded (id) VALUES ($1) RETURNING *", id)
+			assert.Nil(t, err)
+			var len int
+			for rows.Next() {
+				values, err := rows.Values()
+				assert.Nil(t, err)
+				assert.Equal(t, values[0].(int64), int64(id))
+				len += 1
+			}
+			assert.Equal(t, len, 1)
+
+			rows, err = tx.Query(context.Background(), "SELECT * FROM sharded WHERE id = $1", id)
+			assert.Nil(t, err)
+			len = 0
+			for rows.Next() {
+				values, err := rows.Values()
+				assert.Nil(t, err)
+				assert.Equal(t, values[0].(int64), int64(id))
+				len += 1
+			}
+			assert.Equal(t, len, 1)
+			err = tx.Rollback(context.Background())
+			assert.Nil(t, err)
+		}
+	}
 }
